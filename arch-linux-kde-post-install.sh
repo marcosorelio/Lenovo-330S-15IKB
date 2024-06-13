81dcdcc5da1890109127c768b96501fdb71f5c77
Maros Orelio <bw.marcos@gmail.com>
2024-06-13 20:19:49 -0300

    cpu-power

--- arch-linux-kde-post-install.sh
+++ arch-linux-kde-post-install.sh
@@ -7,53 +7,71 @@
 #######################
 
 #Inicial config
+echo "Inicial config -------------"
 sudo systemctl enable --now bluetooth
+sleep 3
 
 #Install git
+echo "Install git -------------"
 yes | sudo pacman -Syu
 yes | sudo pacman -Sy --needed git base-devel
 git config --global user.name "marcosorelio"
 git config --global user.email bw.marcos@gmail.com
+sleep 3
 
 #Install Yay
+echo "Install Yay -------------"
 cd ~
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
+sleep 3
 
 #Install Reflector
+echo "Install Reflector -------------"
 yes | yay -Sy reflector
 reflector --latest 5 --country "United States" --protocol https --sort rate --save /etc/sudo pacman.d/mirrorlist
 #cp /etc/xdg/reflector/reflector.conf /etc/xdg/reflector/reflector.conf.bak
 #echo '--country "United States"' >> /etc/xdg/reflector/reflector.conf
 #systemctl enable --now reflector.service
+sleep 3
 
 #Install tools
+echo "Install Reflector -------------"
 yes | yay -Sy spectacle
 yes | yay -Sy btop
 yes | yay -Sy virtualbox virtualbox-host-dkms linux-lts-headers
 #Fix Error start virtual box
 modprobe vboxdrv
+sleep 3
 
 #Install docker
+echo "Install docker -------------"
 yes | yay -Sy docker docker-compose
 groupadd docker
 usermod -aG docker $USER
 systemctl enable --now docker.service
+sleep 3
 
 #Install asdf lib tools
+echo "Install asdf lib tools -------------"
 yes | yay -Sy asdf-vm
 echo "source /opt/asdf-vm/asdf.sh" >> ~/.bashrc
 source ~/.bashrc
+sleep 3
 
 #asdf install lib Java
+echo "asdf install lib Java -------------"
 asdf plugin add java
 asdf install java openjdk-21
 asdf global java openjdk-21
 java -version
+sleep 3
 
 #Install flatpak
+echo "Install flatpak -------------"
 yes | yay -Sy flatpak
+sleep 3
 
 flatpak install flathub org.mozilla.firefox
 flatpak install flathub org.libreoffice.LibreOffice
---
+++ cpu-power-mode.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+######################
+#@Autor: Marcos Orelio
+#@Date: 2023-11-6
+#@Version: 1.0.0
+#@URL: https://raw.githubusercontent.com/marcosorelio/Arch_Lenovo_330S-15IKB/main/arch-linux-kde-post-install.sh
+#######################
+
+#Inicial config
+
+notify-send -a "CPU Power Mode" "Running now" "Power safe mode"
+
+notify-send -a "CPU Power Mode" "Running now" "Balanced mode"
+
+notify-send -a "CPU Power Mode" "Running now" "Performance mode"
